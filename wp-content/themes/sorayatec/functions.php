3c4cee96b2080933d8117df9cabf0ebef7a926dd
Thomas Mathew <thomas_m@yatnam.co.in>
2021-01-25 17:58:58 +0530

    #14 Made banner content editable and modified affected files during merge

--- wp-content/themes/sorayatec/functions.php
+++ wp-content/themes/sorayatec/functions.php
@@ -70,7 +70,7 @@ add_action( 'wp_enqueue_scripts', 'wphierarchy_enqueue_scripts' );
  function register_navwalker(){
 	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 }
-
+add_action( 'after_setup_theme', 'register_navwalker' );
 
 
 //  Custom Post Type
