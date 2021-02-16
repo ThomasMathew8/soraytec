c33860364070e7ca4ad92a9efb65912c62fcb94d
Thomas Mathew <thomas_m@yatnam.co.in>
2021-02-16 12:09:35 +0530

    #24 Added Mailchimp signup form using template

--- wp-content/themes/sorayatec/assets/js/custom.js
+++ wp-content/themes/sorayatec/assets/js/custom.js
@@ -68,6 +68,13 @@
 });
 
 
+   (function($) 
+   {window.fnames = new Array(); 
+    window.ftypes = new Array();
+    fnames[0]='EMAIL';ftypes[0]='email';}
+    (jQuery));
+    var $mcj = jQuery.noConflict(true);
+
 
     var wid = $(window).width();
     if (wid <= 1600) {
---
+++ wp-content/themes/sorayatec/template-parts/content-signup.php
@@ -0,0 +1,28 @@
+
+<div id="mc_embed_signup">
+    <form action="https://yatnam.us1.list-manage.com/subscribe/post?u=54f2e2729fe8923aed824c89a&amp;id=54abce5cdc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+        
+            <h2>
+                <?php if( get_field( 'signup_heading' ) ): echo get_field( 'signup_heading' ); endif; ?>
+            </h2> 
+
+            <div class="form-group">
+                <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="<?php if( get_field( 'signup_placeholder' ) ): echo get_field( 'signup_placeholder' ); endif; ?>"
+                required="">
+                <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            </div>
+            
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_54f2e2729fe8923aed824c89a_54abce5cdc" tabindex="-1" value=""></div>
+
+            <div class="clear">
+                <input type="submit" value="<?php if( get_field( 'signup_button_text' ) ): echo get_field( 'signup_button_text' ); endif; ?>" name="subscribe" id="mc-embedded-subscribe" class="btn btn-signup">
+            </div>
+
+        </div>
+
+    </form>
+</div>
